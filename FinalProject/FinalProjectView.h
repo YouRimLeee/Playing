b8386291f4698105731c3bfa48ceb8626009b1bd
유림 이 <bu2990@bu.ac.kr>
2020-12-10 01:39:17 +0900

    수정1

---
+++ FinalProject/FinalProjectView.h
@@ -0,0 +1,49 @@
+﻿
+// FinalProjectView.h: CFinalProjectView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CFinalProjectView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CFinalProjectView() noexcept;
+	DECLARE_DYNCREATE(CFinalProjectView)
+
+// 특성입니다.
+public:
+	CFinalProjectDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CFinalProjectView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // FinalProjectView.cpp의 디버그 버전
+inline CFinalProjectDoc* CFinalProjectView::GetDocument() const
+   { return reinterpret_cast<CFinalProjectDoc*>(m_pDocument); }
+#endif
+
