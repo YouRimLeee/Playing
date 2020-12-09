6a8311b8359c5da51dfc1a703a334028a27407f5
유림 이 <bu2990@bu.ac.kr>
2020-12-10 05:02:21 +0900

    수정3

--- FinalProject/Resource.h
+++ FinalProject/Resource.h
@@ -7,7 +7,6 @@
 #define IDR_MAINFRAME                   128
 #define IDR_FinalProjectTYPE            130
 #define IDB_BITMAP1                     310
-#define IDB_BITMAP2                     311
 #define IDB_BIT_FOREST                  311
 #define ID_PLAYING_PARK                 32771
 #define ID_PARK_BUTTERFLY               32772
@@ -18,13 +17,19 @@
 #define ID_PARK_STAY                    32777
 #define ID_PARK_MOVE                    32778
 #define ID_PLAYING_PUZZLE               32779
+#define ID_BUTTERFLY                    32780
+#define ID_CHILD                        32781
+#define ID_STAY                         32782
+#define ID_MOVE                         32783
+#define ID_Playing_MOVE                 32784
+#define ID_Playing_STAY                 32785
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
-#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_COMMAND_VALUE         32786
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
